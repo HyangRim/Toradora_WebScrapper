baa394b683fc27f8cb9ea01666ac1532ee8c42c2
Park in su <eyiuta1@gmail.com>
2021-12-23 17:51:28 +0900

    Delete src directory

--- src/db/mongo_connection.ts
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const ffs = require("fs");
-const express = require("express")
-const Schema = mongoose.Schema;
-const multer = require('multer');
-
--- src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-/*const router = express.Router();
-
-router.get('/aisaka_taiga', function(req, res, next){
-    const ImgUrl = "http://localhost:5000/aisaka_taiga"
-});*/
